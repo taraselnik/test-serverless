75901f967830e744baeb30ab74f74b067c31253e
Taras Pustovyi <taraselnik@gmail.com>
2023-03-01 00:38:26 +0200

    add event to severless function

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,18 @@
 import fetch  from 'node-fetch'
 
-export async function handler () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/2'
+export async function handler (event, context) {
+  const eventBody = JSON.parse(event.body)
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
+
+  console.log(event)
 
   const response = await fetch(POKE_API)
   const data = await response.json()
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
+    body: JSON.stringify({
+      pokemon: data.pokemon_entries
+    })
   }
 }
